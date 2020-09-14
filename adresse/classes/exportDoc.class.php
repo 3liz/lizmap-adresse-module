030a89667fe6a0889885fee0cfa999f93b45813f
Drillin <sio.drillin@gmail.com>
2020-09-14 16:47:00 +0200

    Adding comment in exportBal function

--- adresse/classes/exportDoc.class.php
+++ adresse/classes/exportDoc.class.php
@@ -9,10 +9,14 @@ class exportDoc{
   }
 
   function exportBal($fileName, $result){
+    # Opening file
     $fp = fopen($fileName, 'w');
     $list = array();
+    # Adding encode utf8 to the file
     fprintf($fp, chr(0xEF).chr(0xBB).chr(0xBF));
+    # Adding first CSV line
     fputcsv($fp, array("cle_interop", "uid_adresse", "voie_nom", "numero", "suffixe", "commune_nom", "position","x", "y", "long", "lat", "source", "date_derniere_maj"), ';');
+    # Adding the data in the CSV file
     foreach ($result as $value) {
       fputcsv($fp, array($value->cle_interop, $value->uid_adresse, $value->voie_nom, $value->numero, $value->suffixe, $value->commune_nom, $value->position, $value->x, $value->y, $value->long, $value->lat, $value->source, $value->date_derniere_maj), ';');
     }
