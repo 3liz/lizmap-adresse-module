27f311f9267128d742e26521509a87c2c2ecd5fe
Drillin <sio.drillin@gmail.com>
2020-09-14 12:24:54 +0200

    Adding utf-8 for csv file

--- adresse/classes/exportDoc.class.php
+++ adresse/classes/exportDoc.class.php
@@ -11,6 +11,7 @@ class exportDoc{
   function exportBal($fileName, $result){
     $fp = fopen($fileName, 'w');
     $list = array();
+    fprintf($fp, chr(0xEF).chr(0xBB).chr(0xBF));
     fputcsv($fp, array("cle_interop", "uid_adresse", "voie_nom", "numero", "suffixe", "commune_nom", "position","x", "y", "long", "lat", "source", "date_derniere_maj"), ';');
     foreach ($result as $value) {
       fputcsv($fp, array($value->cle_interop, $value->uid_adresse, $value->voie_nom, $value->numero, $value->suffixe, $value->commune_nom, $value->position, $value->x, $value->y, $value->long, $value->lat, $value->source, $value->date_derniere_maj), ';');
@@ -21,6 +22,7 @@ class exportDoc{
   function exportVoieADelib($fileName, $repository, $project, $result, $com){
     $fp = fopen($fileName, 'w');
     $list = array();
+    fprintf($fp, chr(0xEF).chr(0xBB).chr(0xBF));
     foreach ($com as $value) {
       fputcsv($fp, array($value->cnom. ' - '.$value->cinsee. ' - Nombre de Voies : '. $value->nbid), ';');
     }
