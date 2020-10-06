ffb91aa7b59f2784e0377494c127bd45f57bcbf7
Drillin Pierre <sio.drillin@gmail.com>
2020-10-06 14:41:13 +0200

    BAL: Update field source in CSV

--- adresse/classes/exportDoc.class.php
+++ adresse/classes/exportDoc.class.php
@@ -18,7 +18,7 @@ class exportDoc{
     fputcsv($fp, array("cle_interop", "uid_adresse", "voie_nom", "numero", "suffixe", "commune_nom", "position","x", "y", "long", "lat", "source", "date_derniere_maj"), ';');
     # Adding the data in the CSV file
     foreach ($result as $value) {
-      fputcsv($fp, array($value->cle_interop, $value->uid_adresse, $value->voie_nom, $value->numero, $value->suffixe, $value->commune_nom, $value->position, $value->x, $value->y, $value->long, $value->lat, $value->source, $value->date_derniere_maj), ';');
+      fputcsv($fp, array($value->cle_interop, $value->uid_adresse, $value->voie_nom, $value->numero, $value->suffixe, $value->commune_nom, $value->position, $value->x, $value->y, $value->long, $value->lat, $value->commune_nom, $value->date_derniere_maj), ';');
     }
     fclose($fp);
   }
